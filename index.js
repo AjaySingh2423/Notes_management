e6f375d47f58b1a110a08e37e72c6fd378c6b992
Ajay Singh <ajaysingh17242002@gmail.com>
2026-07-11 16:13:47 +0530

    Fixed error

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:false}));  //It is a middleware
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+    res.redirect("/login");
+});
+
 
 const userRouter = require("./routes/user");
 app.use(userRouter);
